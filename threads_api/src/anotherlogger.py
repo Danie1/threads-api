68e9117ccbe87498daa80a5034618c5607371cbb
Danie1 <me@danie1.me>
2023-07-16 01:00:14 +0300

    refactored some code (#37)

---
+++ threads_api/src/anotherlogger.py
@@ -0,0 +1,32 @@
+import logging
+from colorama import init, Fore, Style
+import json
+
+def is_json_serializable(obj):
+    try:
+        json.dumps(obj)
+        return True
+    except (TypeError, ValueError):
+        return False
+
+def log(*args, **kwargs):
+    log_message = f"{Fore.GREEN}<---- START ---->\n"
+
+    # Collect positional arguments
+    if args:
+        log_message += "Positional arguments:\n"
+        for index, arg in enumerate(args, start=1):
+            log_message += f"  Arg {index}: [{Style.RESET_ALL}{arg}{Fore.GREEN}]\n"
+
+    # Collect keyword arguments
+    if kwargs:
+        log_message += "Keyword arguments:\n"
+        for key, value in kwargs.items():
+            if is_json_serializable(value):
+                value = json.dumps(value, indent=4)
+            log_message += f"  [{key}]: [{Style.RESET_ALL}{value}{Fore.GREEN}]\n"
+
+    log_message += f"<---- END ---->\n{Style.RESET_ALL}"
+
+    # Log the message
+    logging.info(log_message)
